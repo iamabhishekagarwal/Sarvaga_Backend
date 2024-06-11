0a1d1a2de99e25dd347931efe588656ba9c43732
Abhishek Agarwal <iamabhishekagarwal@gmail.com>
2024-06-11 19:14:40 +0530

    Restore

--- dist/Routes/userRoutes.js
+++ dist/Routes/userRoutes.js
@@ -15,11 +15,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const zod_1 = require("zod");
 const client_1 = require("@prisma/client");
-const google_auth_library_1 = require("google-auth-library");
 const routerU = express_1.default.Router();
 const prismaU = new client_1.PrismaClient();
-const dotenv = require('dotenv');
-dotenv.config();
 const userSchema = zod_1.z.object({
     username: zod_1.z.string().email(),
     firstName: zod_1.z.string().min(1),
@@ -55,43 +52,6 @@ function getAllUsers() {
         }
     });
 }
-routerU.post("/", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5172');
-    res.header('Referrer-Policy', 'no-referrer-when-downgrade');
-    const redirectUrl = 'http://localhost:5172/user';
-    const oAuth2Client = new google_auth_library_1.OAuth2Client(process.env.CLIENT_ID, process.env.CLIENT_SECRET, redirectUrl);
-    const authorizeUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: 'https://www.googleapis.com/auth/userinfo.profile openid',
-        prompt: 'consent'
-    });
-    res.json({ url: authorizeUrl });
-}));
-function getUserdata(access_token) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const response = yield fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token${access_token}`);
-        const data = yield response.json();
-        console.log('data', data);
-    });
-}
-routerU.get('/', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    const code = req.query.code;
-    if (typeof code !== 'string') {
-        return res.status(400).send('Invalid code parameter');
-    }
-    try {
-        const redirectUrl = 'http://localhost:5172/user';
-        const oAuth2Client = new google_auth_library_1.OAuth2Client(process.env.CLIENT_ID, process.env.CLIENT_SECRET, redirectUrl);
-        const res = yield oAuth2Client.getToken(code);
-        yield oAuth2Client.setCredentials(res.tokens);
-        console.log("Tokens acquired");
-        const user = oAuth2Client.credentials;
-        yield (getUserdata(user.access_token));
-    }
-    catch (err) {
-        res.json({ "msg": "Error Signing in with google" });
-    }
-}));
 routerU.post('/fetchData', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const users = yield getAllUsers();
--- dist/index.js
+++ dist/index.js
@@ -6,20 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const adminRoutes_1 = __importDefault(require("./Routes/adminRoutes")); // Assuming adminRoutes.ts is the correct filename
 const userRoutes_1 = __importDefault(require("./Routes/userRoutes")); // Assuming userRoutes.ts is the correct filename
-const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const port = 5172;
 app.use(express_1.default.json());
-const corsOptions = {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'application/json'], // Add other headers as needed
-};
-// Apply CORS options
-app.use((0, cors_1.default)(corsOptions));
-// Handle preflight requests for all routes
-app.options('*', (0, cors_1.default)(corsOptions));
 app.use('/admin', adminRoutes_1.default);
 app.use('/user', userRoutes_1.default);
 app.listen(port, () => {
